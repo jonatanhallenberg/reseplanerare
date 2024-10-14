7e9227bffe96f44929d6e664b8c3b8ee8f2ab93e
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2024-10-14 23:24:20 +0200

    add mocked test

--- client/tests/travel-planner.spec.ts
+++ client/tests/travel-planner.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import { searchResultMock } from "./mock/searchResultMock";
 
 test("has title", async ({ page }) => {
   await page.goto("http://localhost:5173/");
@@ -17,4 +18,31 @@ test("snapshot test", async ({ page }) => {
   await page.goto("http://localhost:5173/");
 
   await expect(page).toHaveScreenshot("travel-planner.png");
+});
+
+test("search for a destination", async ({ page }) => {
+  await page.goto("http://localhost:5173/");
+
+  //Mock url independent of searchstring
+  await page.route(
+    "http://localhost:4000/api/location/search?searchString=*",
+    (route) => {
+      route.fulfill({
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify(searchResultMock),
+      });
+    }
+  );
+
+  const from = page.getByTestId("from-autocomplete");
+
+  await from.fill("Falköping");
+
+  //Wait for the autocomplete to show the results
+  await page.waitForTimeout(2000);
+  await from.press("ArrowDown");
+  await from.press("Enter");
+
+  expect(from).toHaveValue("FALKÖPING");
 });
